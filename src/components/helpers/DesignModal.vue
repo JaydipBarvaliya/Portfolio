40c84ebf90dde2d54cdb331fb6db2c22f9495a74
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2023-01-17 11:57:17 -0500

    Carousel removed as it was unused.

--- src/components/helpers/DesignModal.vue
+++ src/components/helpers/DesignModal.vue
@@ -66,13 +66,11 @@
 </template>
 
 <script>
-import Carousel from "./Carousel";
 import Gallery from "./Gallery";
 
 export default {
   name: "Modal",
   components: {
-    Carousel,
     Gallery,
   },
   props: {
--- src/components/helpers/Modal.vue
+++ src/components/helpers/Modal.vue
@@ -74,13 +74,11 @@
 </template>
 
 <script>
-import Carousel from "./Carousel";
 import Gallery from "./Gallery";
 
 export default {
   name: "Modal",
   components: {
-    Carousel,
     Gallery,
   },
   props: {
