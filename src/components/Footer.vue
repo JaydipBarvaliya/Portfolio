a22f4dedd8066855390f7bbb04ea11a263003cd7
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2023-01-31 05:51:57 -0500

    copyright year changed.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,7 @@
           class="col-xl-6 col-bg-6 col-md-6 col-sm-12 pbelow"
           style="color: white;"
         >
-          <span>© 2021 Copyright: Jaydip Barvaliya</span>
+          <span>© 2023 Copyright: Jaydip Barvaliya</span>
         </div>
 
         <div class="col-xl-6 col-bg-6 col-md-6 col-sm-12">
