2a6a05bc84cd85123a1fd9bb3ffebab979b982fa
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2023-01-31 05:13:29 -0500

    Added email.js configuration

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let config = {
   emailjs: {
-    serviceID: "service_zgn3pzg",
-    templateID: "template_0p0xp5t",
-    userID: "user_lvJPo2T7f3piQuVteW4Em"
+    serviceID: "service_iha3l3g",
+    templateID: "template_fi3vo6n",
+    userID: "zLs1l1ZoP2RuwcuXY"
   }
 };
 
